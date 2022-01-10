8400f10b4c05c69b74fa768931de7b7be995a712
Nyk Ma <nykma@mask.io>
2022-01-10 17:03:46 +0800

    [!!] GET /v1/proof: query with imit

--- controller/proof_query.go
+++ controller/proof_query.go
@@ -10,6 +10,10 @@ import (
 	"golang.org/x/xerrors"
 )
 
+const (
+	QUERY_LIMIT = 20
+)
+
 type ProofQueryRequest struct {
 	Platform string `form:"platform"`
 	Identity []string       `form:"identity"`
@@ -55,6 +59,7 @@ func performProofQuery(req ProofQueryRequest) []ProofQueryResponseSingle {
 	switch (req.Platform) {
 	case string(types.Platforms.NextID): {
 		tx = tx.Where("persona IN ?", req.Identity).
+			Limit(QUERY_LIMIT).
 			Find(&proofs)
 	}
 	case "": { // All platform
@@ -66,7 +71,7 @@ func performProofQuery(req ProofQueryRequest) []ProofQueryResponseSingle {
 			tx = tx.Or("identity LIKE ?", "%"+strings.ToLower(id)+"%")
 		}
 
-		tx = tx.Find(&proofs)
+		tx = tx.Limit(QUERY_LIMIT).Find(&proofs)
 	}
 	default: {
 		tx = tx.Where("platform", req.Platform).
@@ -78,7 +83,7 @@ func performProofQuery(req ProofQueryRequest) []ProofQueryResponseSingle {
 			}
 			tx = tx.Or("identity LIKE ?", "%"+strings.ToLower(id)+"%")
 		}
-		tx = tx.Find(&proofs)
+		tx = tx.Limit(QUERY_LIMIT).Find(&proofs)
 	}
 	}
 	if tx.Error != nil || tx.RowsAffected == int64(0) || len(proofs) == 0 {
@@ -87,13 +92,13 @@ func performProofQuery(req ProofQueryRequest) []ProofQueryResponseSingle {
 
 
 	// proofs.group_by(&:persona)
-	persona_proof_map := make(map[string][]*model.Proof, 0)
+	persona_proof_map := make(map[string][]model.Proof, 0)
 	for _, p := range proofs {
 		persona_proof, ok := persona_proof_map[p.Persona]
 		if ok {
-			persona_proof_map[p.Persona] = append(persona_proof, &p)
+			persona_proof_map[p.Persona] = append(persona_proof, p)
 		} else {
-			persona_proof_map[p.Persona] = append(make([]*model.Proof, 0, 0), &p)
+			persona_proof_map[p.Persona] = append(make([]model.Proof, 0, 0), p)
 		}
 	}
 
