2eae849aa4f8ca00ed34def948cdb2825395646c
Nyk Ma <nykma@mask.io>
2022-01-17 17:19:09 +0800

    [#][API] GET /healthz: add supported_platforms list

--- controller/main.go
+++ controller/main.go
@@ -4,6 +4,7 @@ import (
 	"net/http"
 
 	"github.com/gin-gonic/gin"
+	"github.com/nextdotid/proof-server/validator"
 )
 
 var (
@@ -16,11 +17,7 @@ type ErrorResponse struct {
 
 func Init() {
 	Engine = gin.Default()
-	Engine.GET("/healthz", func(c *gin.Context) {
-		c.JSON(http.StatusOK, gin.H{
-			"hello": "proof server",
-		})
-	})
+	Engine.GET("/healthz", healthz)
 	Engine.POST("/v1/proof/payload", proofPayload)
 	Engine.POST("/v1/proof", proofUpload)
 	Engine.GET("/v1/proof", proofQuery)
@@ -29,5 +26,17 @@ func Init() {
 func errorResp(c *gin.Context, error_code int, err error) {
 	c.JSON(error_code, ErrorResponse{
 		Message: err.Error(),
+	})
+}
+
+func healthz(c *gin.Context) {
+	supported_platforms := make([]string, 0)
+	for p, _ := range validator.PlatformFactories {
+		supported_platforms = append(supported_platforms, string(p))
+	}
+
+	c.JSON(http.StatusOK, gin.H{
+		"hello": "proof server",
+		"supported_platforms": supported_platforms,
 	})
 }
