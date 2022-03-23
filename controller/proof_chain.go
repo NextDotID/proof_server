15ccc64a3cb9981ddd2a46b8e4ca3e5969dab3d7
fengshanshan <fengshanshn@icloud.com>
2022-03-23 16:45:39 +0800

    optimize for proof chain api && add doc

--- controller/proof_chain.go
+++ controller/proof_chain.go
@@ -3,10 +3,8 @@ package controller
 import (
 	"github.com/gin-gonic/gin"
 	"github.com/nextdotid/proof-server/model"
-	"github.com/nextdotid/proof-server/types"
 	"golang.org/x/xerrors"
 	"net/http"
-	"strconv"
 )
 
 type ProofChainRequest struct {
@@ -16,18 +14,7 @@ type ProofChainRequest struct {
 
 type ProofChainResponse struct {
 	Pagination  ProofChainPaginationResponse `json:"pagination"`
-	ProofChains []ProofItem                  `form:"links"`
-}
-
-type ProofItem struct {
-	ID            int64          `json:"id"`
-	PrevID        int64          `json:"prev_id"`
-	Action        types.Action   `json:"action"`
-	Platform      types.Platform `json:"platform"`
-	Identity      string         `json:"identity"`
-	ProofLocation string         `json:"proof_location"`
-	CreatedAt     string         `json:"created_at"`
-	Signature     string         `json:"signature"`
+	ProofChains []model.ProofChainItem       `json:"proof_chain"`
 }
 
 type ProofChainPaginationResponse struct {
@@ -48,15 +35,19 @@ func proofChainQuery(c *gin.Context) {
 		return
 	}
 
-	list, pagination := performProofChainQuery(req)
+	list, pagination, err := performProofChainQuery(req)
+	if err != nil {
+		errorResp(c, http.StatusInternalServerError, xerrors.Errorf("Error in DB: %w", err))
+		return
+	}
+
 	c.JSON(http.StatusOK, ProofChainResponse{
 		Pagination:  pagination,
 		ProofChains: list,
 	})
-
 }
 
-func performProofChainQuery(req ProofChainRequest) ([]ProofItem, ProofChainPaginationResponse) {
+func performProofChainQuery(req ProofChainRequest) ([]model.ProofChainItem, ProofChainPaginationResponse, error) {
 	pagination := ProofChainPaginationResponse{
 		Total:   0,
 		Per:     PER_PAGE,
@@ -68,33 +59,11 @@ func performProofChainQuery(req ProofChainRequest) ([]ProofItem, ProofChainPagin
 	}
 	offsetCount := pagination.Per * (pagination.Current - 1)
 
-	rs := make([]ProofItem, 0, 0)
-	proofs := make([]model.ProofChain, 0, 0)
-	tx := model.DB.Model(&model.ProofChain{})
-	tx = tx.Where("persona = ?", req.PersonaPubkeyHex)
-
-	countTx := tx // Value-copy another query for total amount calculation
-	countTx.Count(&pagination.Total)
-	if pagination.Total > int64(pagination.Per*pagination.Current) {
+	total, rs, err := model.ProofChainFindByPersona(req.PersonaPubkeyHex, false, offsetCount, pagination.Per)
+	pagination.Total = total
+	if total > int64(pagination.Per*pagination.Current) {
 		pagination.Next = pagination.Current + 1
 	}
 
-	tx = tx.Offset(offsetCount).Limit(pagination.Per).Find(&proofs)
-	if tx.Error != nil || tx.RowsAffected == int64(0) || len(proofs) == 0 {
-		return rs, pagination
-	}
-
-	for _, item := range proofs {
-		rs = append(rs, ProofItem{
-			ID:            item.ID,
-			PrevID:        item.PreviousID.Int64,
-			Action:        item.Action,
-			Platform:      item.Platform,
-			Identity:      item.Identity,
-			ProofLocation: item.Location,
-			CreatedAt:     strconv.FormatInt(item.CreatedAt.Unix(), 10),
-			Signature:     item.Signature,
-		})
-	}
-	return rs, pagination
+	return rs, pagination, err
 }
