a0b47d6539cf8e3c6fa5100c7a9770a65efbaab1
fengshanshan <fengshanshn@icloud.com>
2022-03-23 17:28:07 +0800

    add patination test case for proofchain

--- controller/proof_chain_test.go
+++ controller/proof_chain_test.go
@@ -9,8 +9,9 @@ import (
 
 func Test_proofChainQuery(t *testing.T) {
 	before_each(t)
-	insert_proof(t)
+
 	t.Run("success", func(t *testing.T) {
+		insert_proof(t)
 		resp_body := ProofChainResponse{}
 		resp := APITestCall(
 			Engine,
@@ -24,7 +25,7 @@ func Test_proofChainQuery(t *testing.T) {
 		assert.Equal(t, 2, len(resp_body.ProofChains))
 	})
 
-	t.Run("failure", func(t *testing.T) {
+	t.Run("empty result", func(t *testing.T) {
 		resp_body := ProofChainResponse{}
 		resp := APITestCall(
 			Engine,
@@ -36,5 +37,31 @@ func Test_proofChainQuery(t *testing.T) {
 		assert.Equal(t, 200, resp.Code)
 		//t.Logf("%s", resp.Body.String())
 		assert.Equal(t, 0, len(resp_body.ProofChains))
+	})
+
+	t.Run("patination", func(t *testing.T) {
+		for i := 0; i < 22; i++ { // Create 44 records
+			insert_proof(t)
+		}
+		url := fmt.Sprintf("/v1/proofchain?public_key=%s", persona)
+
+		resp_page1 := ProofChainResponse{} // Page not given
+		APITestCall(Engine, "GET", url, nil, &resp_page1)
+		assert.Equal(t, int64(44), resp_page1.Pagination.Total)
+		assert.Equal(t, 1, resp_page1.Pagination.Current)
+		assert.Equal(t, 2, resp_page1.Pagination.Next)
+		assert.Equal(t, PER_PAGE, len(resp_page1.ProofChains))
+
+		resp_page3 := ProofChainResponse{} // Last page
+		APITestCall(Engine, "GET", url+"&page=3", nil, &resp_page3)
+		assert.Equal(t, 3, resp_page3.Pagination.Current)
+		assert.Equal(t, 0, resp_page3.Pagination.Next)
+		assert.Equal(t, 4, len(resp_page3.ProofChains))
+
+		resp_page4 := ProofChainResponse{} // Page overflow
+		APITestCall(Engine, "GET", url+"&page=4", nil, &resp_page4)
+		assert.Equal(t, 4, resp_page4.Pagination.Current)
+		assert.Equal(t, 0, resp_page4.Pagination.Next)
+		assert.Equal(t, 0, len(resp_page4.ProofChains))
 	})
 }
