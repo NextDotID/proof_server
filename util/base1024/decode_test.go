93ff1205d720a1c1c3a3f4961b2c39a43317c3ac
fengshanshan <fengshanshn@icloud.com>
2022-04-26 10:03:26 +0800

    delete debug code

--- util/base1024/decode_test.go
+++ util/base1024/decode_test.go
@@ -1,7 +1,6 @@
 package base1024
 
 import (
-	"fmt"
 	"github.com/stretchr/testify/assert"
 	"testing"
 )
@@ -17,17 +16,13 @@ func TestDecodeString(t *testing.T) {
 	t.Run("Not Equal", func(t *testing.T) {
 		str := "🐟🔂🏁🤖💧"
 		res, err := DecodeString(str)
-		fmt.Println(string(res), err)
 		assert.Nil(t, err)
 		assert.NotEqual(t, "Maskbook", string(res))
 	})
 
 	t.Run("Decode Playload", func(t *testing.T) {
 		str := "👲🍚🍾🔆🏠🚱👧🦢🕟🛷🔭💘😝🙂🚳🦜🔙🔊🚗🏏👪🛹🗣🏳🦐🥫🦺🚎🕗🚷💡🚁🎟🗯📰🐊🕳🥠💐🎛🤵🆘🔣📥🦝🔉🌊🥠🥅🍏🥜🃏"
-		//"👲🍚🍾🔆🏠🚱👧\U0001F9A2🕟🛷🔭💘😝🙂🚳\U0001F99C🔙🔊🚗🏏👪\U0001F6F9🗣🏳🦐🥫\U0001F9BA🚎🕗🚷💡🚁🎟🗯📰🐊🕳🥠💐🎛🤵🆘🔣📥\U0001F99D🔉🌊🥠🥅🍏🥜🃏"
-		//str := "👲🍚🍾🔆🏠🚱👧\U0001F9A2🕟🛷🔭💘😝🙂🚳\U0001F99C🔙🔊🚗🏏👪\U0001F6F9🗣🏳🦐🥫\U0001F9BA🚎🕗🚷💡🚁🎟🗯📰🐊🕳🥠💐🎛🤵🆘🔣📥\U0001F99D🔉🌊🥠🥅🍏🥜🃏"
 		res, err := DecodeString(str)
-		fmt.Println(string(res), err)
 		assert.Nil(t, err)
 		assert.NotEqual(t, "Maskbook", string(res))
 	})
