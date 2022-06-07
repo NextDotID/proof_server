2730b38034db33de929c576c4c015237c9d6f3fa
Mitch Hwang <mitchx7@outlook.com>
2022-06-07 09:57:47 +0800

    feat(solana): implement validator

--- go.mod
+++ go.mod
@@ -16,6 +16,8 @@ require (
 )
 
 require (
+	contrib.go.opencensus.io/exporter/stackdriver v0.13.4 // indirect
+	filippo.io/edwards25519 v1.0.0-rc.1 // indirect
 	github.com/aws/aws-sdk-go-v2/credentials v1.12.0 // indirect
 	github.com/aws/aws-sdk-go-v2/feature/ec2/imds v1.12.4 // indirect
 	github.com/aws/aws-sdk-go-v2/internal/configsources v1.1.10 // indirect
@@ -25,18 +27,26 @@ require (
 	github.com/aws/aws-sdk-go-v2/service/sso v1.11.4 // indirect
 	github.com/aws/aws-sdk-go-v2/service/sts v1.16.4 // indirect
 	github.com/aws/smithy-go v1.11.2 // indirect
+	github.com/aybabtme/rgbterm v0.0.0-20170906152045-cc83f3b3ce59 // indirect
+	github.com/blendle/zapdriver v1.3.1 // indirect
 	github.com/btcsuite/btcd v0.22.0-beta // indirect
 	github.com/btcsuite/btcd/btcec/v2 v2.1.3 // indirect
 	github.com/cenkalti/backoff/v4 v4.1.3 // indirect
 	github.com/davecgh/go-spew v1.1.1 // indirect
 	github.com/decred/dcrd/dcrec/secp256k1/v4 v4.0.1 // indirect
+	github.com/dfuse-io/logging v0.0.0-20210109005628-b97a57253f70 // indirect
 	github.com/dghubble/sling v1.4.0 // indirect
+	github.com/fatih/color v1.13.0 // indirect
 	github.com/fsnotify/fsnotify v1.5.3 // indirect
+	github.com/gagliardetto/binary v0.6.1 // indirect
+	github.com/gagliardetto/solana-go v1.4.0 // indirect
+	github.com/gagliardetto/treeout v0.1.4 // indirect
 	github.com/gin-contrib/sse v0.1.0 // indirect
 	github.com/go-playground/locales v0.14.0 // indirect
 	github.com/go-playground/universal-translator v0.18.0 // indirect
 	github.com/go-playground/validator/v10 v10.10.1 // indirect
 	github.com/go-sql-driver/mysql v1.6.0 // indirect
+	github.com/golang/groupcache v0.0.0-20210331224755-41bb18bfe9da // indirect
 	github.com/golang/protobuf v1.5.2 // indirect
 	github.com/google/go-querystring v1.1.0 // indirect
 	github.com/gorilla/websocket v1.5.0 // indirect
@@ -52,12 +62,18 @@ require (
 	github.com/jinzhu/inflection v1.0.0 // indirect
 	github.com/jinzhu/now v1.1.5 // indirect
 	github.com/json-iterator/go v1.1.12 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
 	github.com/leodido/go-urn v1.2.1 // indirect
+	github.com/logrusorgru/aurora v2.0.3+incompatible // indirect
 	github.com/magiconair/properties v1.8.6 // indirect
+	github.com/mattn/go-colorable v0.1.12 // indirect
 	github.com/mattn/go-isatty v0.0.14 // indirect
+	github.com/mitchellh/go-testing-interface v1.14.1 // indirect
 	github.com/mitchellh/mapstructure v1.5.0 // indirect
 	github.com/modern-go/concurrent v0.0.0-20180306012644-bacd9c7ef1dd // indirect
 	github.com/modern-go/reflect2 v1.0.2 // indirect
+	github.com/mostynb/zstdpool-freelist v0.0.0-20201229113212-927304c0c3b1 // indirect
+	github.com/mr-tron/base58 v1.2.0 // indirect
 	github.com/pelletier/go-toml v1.9.5 // indirect
 	github.com/pelletier/go-toml/v2 v2.0.0-beta.8 // indirect
 	github.com/pmezard/go-difflib v1.0.0 // indirect
@@ -66,10 +82,19 @@ require (
 	github.com/spf13/jwalterweatherman v1.1.0 // indirect
 	github.com/spf13/pflag v1.0.5 // indirect
 	github.com/subosito/gotenv v1.2.0 // indirect
+	github.com/teris-io/shortid v0.0.0-20201117134242-e59966efd125 // indirect
+	github.com/tidwall/gjson v1.9.3 // indirect
+	github.com/tidwall/match v1.1.1 // indirect
+	github.com/tidwall/pretty v1.2.0 // indirect
 	github.com/ugorji/go/codec v1.2.7 // indirect
+	go.opencensus.io v0.23.0 // indirect
+	go.uber.org/atomic v1.7.0 // indirect
+	go.uber.org/multierr v1.6.0 // indirect
+	go.uber.org/zap v1.16.0 // indirect
 	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 	golang.org/x/exp v0.0.0-20220414153411-bcd21879b8fd // indirect
 	golang.org/x/net v0.0.0-20220425223048-2871e0cb64e4 // indirect
+	golang.org/x/term v0.0.0-20210927222741-03fcf44c2211 // indirect
 	golang.org/x/text v0.3.7 // indirect
 	google.golang.org/protobuf v1.28.0 // indirect
 	gopkg.in/ini.v1 v1.66.4 // indirect
---
+++ validator/solana/solana_test.go
@@ -0,0 +1,172 @@
+package solana
+
+import (
+	"crypto/ecdsa"
+	"strings"
+	"testing"
+	"time"
+
+	"github.com/gagliardetto/solana-go"
+	"github.com/google/uuid"
+	"github.com/mr-tron/base58"
+	"github.com/nextdotid/proof-server/config"
+	"github.com/nextdotid/proof-server/types"
+	"github.com/nextdotid/proof-server/validator"
+
+	mycrypto "github.com/nextdotid/proof-server/util/crypto"
+	"github.com/sirupsen/logrus"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	personaPriv *ecdsa.PrivateKey
+	walletPriv  solana.PrivateKey
+)
+
+func before_each(t *testing.T) {
+	logrus.SetLevel(logrus.DebugLevel)
+	config.Init("../../config/config.test.json")
+}
+
+func generate() Solana {
+	sol := Solana{
+		Base: &validator.Base{
+			Platform: types.Platforms.Solana,
+			Previous: "",
+			Action:   types.Actions.Create,
+			Extra: map[string]string{
+				"wallet_signature": "",
+			},
+			CreatedAt: time.Now(),
+			Uuid:      uuid.New(),
+		},
+	}
+	_, personaPriv = mycrypto.GenerateKeypair()
+	sol.Pubkey = &personaPriv.PublicKey
+
+	walletPriv, _ = solana.NewRandomPrivateKey()
+
+	// Base58 encoded
+	sol.Identity = walletPriv.PublicKey().String()
+
+	// Generate sig
+	payloadBytes := []byte(sol.GenerateSignPayload())
+	sol.Signature, _ = mycrypto.SignPersonal(payloadBytes, personaPriv)
+	walletSig, _ := walletPriv.Sign(payloadBytes)
+	sol.Extra = map[string]string{
+		// Base58 encoded
+		"wallet_signature": walletSig.String(),
+	}
+
+	return sol
+}
+
+func Test_GeneratePostPayload(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		before_each(t)
+		sol := generate()
+		assert.Equal(t, "", sol.GeneratePostPayload()["default"])
+	})
+}
+
+func Test_GenerateSignPayload(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		result := sol.GenerateSignPayload()
+		assert.Contains(t, result, "\"identity\":\""+walletPriv.PublicKey().String())
+		assert.NotContains(t, result, "\"identity\":\""+strings.ToLower(walletPriv.PublicKey().String()))
+		assert.Contains(t, result, "\"persona\":\"0x"+mycrypto.CompressedPubkeyHex(sol.Pubkey))
+		assert.Contains(t, result, "\"platform\":\"solana\"")
+	})
+}
+
+func Test_Validate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		assert.NoError(t, sol.Validate())
+	})
+
+	t.Run("fail with wrong wallet signature", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		sol.Extra = map[string]string{
+			"wallet_signature": base58.Encode([]byte("{}")),
+		}
+		sol.Signature, _ = mycrypto.SignPersonal([]byte(sol.GenerateSignPayload()), personaPriv)
+
+		assert.Error(t, sol.Validate())
+	})
+
+	t.Run("fail with wrong persona signature", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		walletSig, _ := walletPriv.Sign([]byte(sol.GenerateSignPayload()))
+		sol.Extra = map[string]string{
+			"wallet_signature": walletSig.String(),
+		}
+		sol.Signature = []byte(uuid.New().String())
+
+		assert.Error(t, sol.Validate())
+	})
+}
+
+func Test_Validate_Delete(t *testing.T) {
+	t.Run("signed by persona", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		sol.Action = types.Actions.Delete
+		sol.Extra = map[string]string{
+			"wallet_signature": "",
+		}
+		sol.Signature, _ = mycrypto.SignPersonal([]byte(sol.GenerateSignPayload()), personaPriv)
+
+		assert.NoError(t, sol.Validate())
+	})
+
+	t.Run("signed by wallet", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		sol.Action = types.Actions.Delete
+		walletSig, _ := walletPriv.Sign([]byte(sol.GenerateSignPayload()))
+		sol.Extra = map[string]string{
+			"wallet_signature": walletSig.String(),
+		}
+
+		assert.NoError(t, sol.Validate())
+	})
+
+	t.Run("signed by persona, but with wrong wallet_signature", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		sol.Action = types.Actions.Delete
+		sol.Signature, _ = mycrypto.SignPersonal([]byte(sol.GenerateSignPayload()), personaPriv)
+		sol.Extra = map[string]string{
+			"wallet_signature": base58.Encode([]byte(uuid.New().String())),
+		}
+
+		assert.Error(t, sol.Validate())
+	})
+
+	t.Run("signed by wallet, but with wrong persona sig, which should be ok", func(t *testing.T) {
+		before_each(t)
+
+		sol := generate()
+		sol.Action = types.Actions.Delete
+		walletSig, _ := walletPriv.Sign([]byte(sol.GenerateSignPayload()))
+		sol.Signature = []byte(uuid.New().String())
+		sol.Extra = map[string]string{
+			"wallet_signature": walletSig.String(),
+		}
+
+		assert.NoError(t, sol.Validate())
+	})
+}
