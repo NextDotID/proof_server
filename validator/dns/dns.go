766e0e7b58601082176951034c023e600794790c
Nyk Ma <nykma@mask.io>
2022-09-27 16:47:33 +0800

    [#] validator/dns

--- validator/dns/dns.go
+++ validator/dns/dns.go
@@ -1,13 +1,21 @@
 package dns
 
 import (
+	"encoding/base64"
 	"encoding/json"
 	"fmt"
 	"io"
 	"net/http"
+	"strings"
+	"time"
 
+	"github.com/google/uuid"
 	"github.com/nextdotid/proof_server/types"
+	"github.com/nextdotid/proof_server/util"
+	"github.com/nextdotid/proof_server/util/crypto"
 	"github.com/nextdotid/proof_server/validator"
+	"github.com/samber/lo"
+	"github.com/sirupsen/logrus"
 	"golang.org/x/xerrors"
 )
 
@@ -28,11 +36,11 @@ type DOHResponse struct {
 	// If true, it means that every record in the answer was verified with DNSSEC.
 	AD bool `json:"AD"`
 	// If true, the client asked to disable DNSSEC validation. In this case, Cloudflare will still fetch the DNSSEC-related records, but it will not attempt to validate the records.
-	CD        bool          `json:"CD"`
-	Question  []DOHQuestion `json:"Question"`
+	CD       bool          `json:"CD"`
+	Question []DOHQuestion `json:"Question"`
 	// If Answer is empty, this field will appear.
-	Authority *[]DOHAnswer  `json:"Authority"`
-	Answer    *[]DOHAnswer  `json:"Answer"`
+	Authority *[]DOHAnswer `json:"Authority"`
+	Answer    *[]DOHAnswer `json:"Answer"`
 }
 
 type DOHQuestion struct {
@@ -53,9 +61,25 @@ type DOHAnswer struct {
 	Data string `json:"data"`
 }
 
+type TXTPayload struct {
+	Version   uint
+	Signature string
+	CreatedAt time.Time
+	uuid      uuid.UUID
+	Previous  *string
+}
+
 type DNS struct {
 	*validator.Base
 }
+
+const (
+	TXT_PAYLOAD_V1 = "ps:true;v:1;sig:%s;ca:%d;uuid:%s;prev:%s"
+)
+
+var (
+	l = logrus.WithFields(logrus.Fields{"module": "validator", "validator": "dns"})
+)
 
 func Init() {
 	if validator.PlatformFactories == nil {
@@ -69,21 +93,66 @@ func Init() {
 }
 
 func (dns *DNS) GeneratePostPayload() (post map[string]string) {
-	return map[string]string{} // TODO
+	var previous string
+	if dns.Previous != "" {
+		previous = dns.Previous
+	} else {
+		previous = "null"
+	}
+	return map[string]string{
+		"default": fmt.Sprintf(TXT_PAYLOAD_V1, "%SIG_BASE64%", dns.CreatedAt.Unix(), dns.Uuid.String(), previous),
+	}
 }
 
 func (dns *DNS) GenerateSignPayload() (payload string) {
-	return "" // TODO
+	payloadStruct := validator.H{
+		"action":     string(dns.Action),
+		"identity":   strings.ToLower(dns.Identity),
+		"platform":   string(types.Platforms.DNS),
+		"prev":       nil,
+		"created_at": util.TimeToTimestampString(dns.CreatedAt),
+		"uuid":       dns.Uuid.String(),
+	}
+	if dns.Previous != "" {
+		payloadStruct["prev"] = dns.Previous
+	}
+	payloadBytes, err := json.Marshal(payloadStruct)
+	if err != nil {
+		l.Warnf("Error when marshaling struct: %s", err.Error())
+		return ""
+	}
+
+	return string(payloadBytes)
 }
 
 func (dns *DNS) Validate() (err error) {
-	return nil // TODO
+	// domain name is case-insensitive
+	dns.Identity = strings.ToLower(dns.Identity)
+	dns.SignaturePayload = dns.GenerateSignPayload()
+	query_resp, err := query(dns.Identity)
+	if err != nil {
+		return err
+	}
+	txt, found := lo.Find(*query_resp.Answer, func(txt DOHAnswer) bool {
+		result, parse_err := parseTxt(txt.Data)
+		return parse_err == nil && result.uuid == dns.Uuid
+	})
+	if !found {
+		return xerrors.New("matched TXT record not found.")
+	}
+	payload, _ := parseTxt(txt.Data)
+	dns.Text = txt.Data
+	dns.Signature, err = base64.StdEncoding.DecodeString(payload.Signature)
+	if err != nil {
+		return xerrors.New("sig in TXT record cannot be recognized.")
+	}
+
+	return crypto.ValidatePersonalSignature(dns.SignaturePayload, dns.Signature, dns.Pubkey)
 }
 
 func query(domain string) (doh_response *DOHResponse, err error) {
 	req, err := http.NewRequest("GET", fmt.Sprintf(DOH, domain), nil)
 	req.Header.Set("Accept", "application/dns-json")
-
 	resp, err := (&http.Client{}).Do(req)
 	if err != nil {
 		return nil, err
@@ -107,4 +176,53 @@ func query(domain string) (doh_response *DOHResponse, err error) {
 	}
 
 	return doh_response, nil
+}
+
+func parseTxt(txtField string) (result TXTPayload, err error) {
+	kv := make(map[string]string)
+
+	// txtField = "\"ps:true;v:1;sig:3QgQUPrPiBloBev8uf1wyjpa4roK4xjN2OXeBpqQFYMOHFo+blMR0Ppyc/JVj0jtdLDGBTrOFdOJPMfvUXZkwAE=;ca:1664267795;uuid:80c98711-f4f6-43c7-b05c-8d86372f6131;prev:null\""
+	lo.ForEach(strings.Split(strings.Trim(txtField, "\""), ";"), func(combined string, i int) {
+		pair := strings.Split(combined, ":")
+		if len(pair) != 2 {
+			err = xerrors.Errorf("TXT payload format error in %s", combined)
+			return
+		}
+		kv[pair[0]] = pair[1]
+	})
+	if err != nil {
+		return TXTPayload{}, err
+	}
+	if !lo.Every(lo.Keys(kv), []string{"ps", "v", "sig", "ca", "uuid", "prev"}) {
+		return TXTPayload{}, xerrors.New("TXT payload not recognized: field missing")
+	}
+	if kv["ps"] != "true" {
+		return TXTPayload{}, xerrors.New("TXT payload not recognized")
+	}
+	if kv["v"] != "1" {
+		return TXTPayload{}, xerrors.New("TXT payload version not recognized")
+	}
+
+	uuid, err := uuid.Parse(kv["uuid"])
+	if err != nil {
+		return TXTPayload{}, err
+	}
+	createdAt, err := util.TimestampStringToTime(kv["ca"])
+	if err != nil {
+		return TXTPayload{}, err
+	}
+
+	var previous *string = nil
+	if kv["prev"] != "null" {
+		prev := kv["prev"]
+		previous = &prev
+	}
+
+	return TXTPayload{
+		Version:   1,
+		Signature: kv["sig"],
+		CreatedAt: createdAt,
+		uuid:      uuid,
+		Previous:  previous,
+	}, nil
 }
--- validator/dns/dns_test.go
+++ validator/dns/dns_test.go
@@ -1,16 +1,102 @@
 package dns
 
 import (
+	"strconv"
 	"testing"
 
+	"github.com/google/uuid"
+	"github.com/nextdotid/proof_server/types"
+	"github.com/nextdotid/proof_server/util"
+	"github.com/nextdotid/proof_server/util/crypto"
+	"github.com/nextdotid/proof_server/validator"
 	"github.com/stretchr/testify/require"
 )
 
-func Test_query(t *testing.T) {
-	_, err := query("nonexist.example.com")
-	require.Error(t, err)
+func build() DNS {
+	pk, _ := crypto.StringToPubkey("0x028568e07ebf497b07a30f8a9d1731980736a4fac9d7c9c9b5682cb82dd3e774d7")
+	createdAt, _ := util.TimestampStringToTime("1664267795")
+	return DNS{
+		Base: &validator.Base{
+			Platform:         types.Platforms.DNS,
+			Previous:         "",
+			Action:           types.Actions.Create,
+			Pubkey:           pk,
+			Identity:         "testcase.nextnext.id",
+			CreatedAt:        createdAt,
+			Uuid:             uuid.MustParse("80c98711-f4f6-43c7-b05c-8d86372f6131"),
+		},
+	}
+}
 
-	body, err := query("example.com")
-	require.NoError(t, err)
-	require.NotNil(t, body)
+func build_invalid() DNS {
+	dns := build()
+	dns.Identity = "testcase_invalid.nextnext.id"
+	return dns
+}
+
+func Test_query(t *testing.T) {
+	t.Run("not found", func(t *testing.T) {
+		_, err := query("nonexist.example.com")
+		t.Log(err.Error())
+		require.Error(t, err)
+	})
+
+	t.Run("found", func(t *testing.T) {
+		body, err := query("example.com")
+		require.NoError(t, err)
+		require.NotNil(t, body)
+		require.NotNil(t, body.Answer)
+	})
+}
+
+func Test_parseTxt(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		txt := "ps:true;v:1;sig:Oyist/0E0MJ5sN3TI33P4EMBGTaCk2S3IQKzYfI5zxpwE2VdHClgLXfmj0L2dPydF8KOXyjbWWuM2AHKdW2DnwE=;ca:1664263102;uuid:26de5ec2-889e-4f59-9fac-dfa8d99e7ce7;prev:null"
+		result, err := parseTxt(txt)
+		require.NoError(t, err)
+		require.Equal(t, "Oyist/0E0MJ5sN3TI33P4EMBGTaCk2S3IQKzYfI5zxpwE2VdHClgLXfmj0L2dPydF8KOXyjbWWuM2AHKdW2DnwE=", result.Signature)
+		require.Nil(t, result.Previous)
+	})
+
+	t.Run("struct field missing", func(t *testing.T) {
+		txt := "ps:true;v:1;sig:Oyist/0E0MJ5sN3TI33P4EMBGTaCk2S3IQKzYfI5zxpwE2VdHClgLXfmj0L2dPydF8KOXyjbWWuM2AHKdW2DnwE=;ca:1664263102;uuid:26de5ec2-889e-4f59-9fac-dfa8d99e7ce7"
+		_, err := parseTxt(txt)
+		require.Error(t, err)
+	})
+}
+
+func Test_GeneratePostPayload(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		dns := build()
+		payload_map := dns.GeneratePostPayload()
+		payload := payload_map["default"]
+		require.Contains(t, payload, dns.Uuid.String())
+		require.Contains(t, payload, strconv.FormatInt(dns.CreatedAt.Unix(), 10))
+		require.Contains(t, payload, "%SIG_BASE64%")
+		require.Contains(t, payload, "prev:null")
+	})
+}
+
+func Test_GenerateSignPayload(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		dns := build()
+		sp := dns.GenerateSignPayload()
+		require.Contains(t, sp, types.Platforms.DNS)
+		require.Contains(t, sp, types.Actions.Create)
+		require.Contains(t, sp, dns.Uuid.String())
+	})
+}
+
+func Test_Validate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		dns := build()
+		require.NoError(t, dns.Validate())
+	})
+
+	t.Run("invalid", func(t *testing.T) {
+		dns := build_invalid()
+		err := dns.Validate()
+		require.Error(t, err)
+		t.Log(err.Error())
+	})
 }
