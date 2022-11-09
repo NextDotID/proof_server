d159d019aeeb17d91bd4154bc290d77e5f869d60
synycboom <synycboom@gmail.com>
2022-11-10 00:49:23 +0700

    refactor: clean up unused code

--- headless/headless.go
+++ headless/headless.go
@@ -6,7 +6,6 @@ import (
 	"github.com/gin-contrib/cors"
 	"github.com/gin-gonic/gin"
 	"github.com/nextdotid/proof_server/common"
-	"github.com/nextdotid/proof_server/validator"
 	"github.com/sirupsen/logrus"
 )
 
@@ -34,11 +33,6 @@ func Init(launcherPath string) {
 }
 
 func healthz(c *gin.Context) {
-	platforms := make([]string, 0)
-	for p := range validator.PlatformFactories {
-		platforms = append(platforms, string(p))
-	}
-
 	c.JSON(http.StatusOK, gin.H{
 		"hello":       "proof service",
 		"environment": common.Environment,
