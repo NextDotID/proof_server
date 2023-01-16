c57511c3d2de9973cbebaea8d329064f8e48679a
Ashfaqur Rahman Sarker <ashfaqurrahmansarker2@gmail.com>
2023-01-16 20:19:48 +0600

    Update main.go

--- config/main.go
+++ config/main.go
@@ -32,6 +32,7 @@ type PlatformConfig struct {
 	Telegram TelegramPlatformConfig `json:"telegram"`
 	Ethereum EthereumPlatformConfig `json:"ethereum"`
 	Discord  DiscordPlatformConfig  `json:"discord"`
+        Line     LinePlatformConfig     `json:"line"`
 }
 
 type ArweaveConfig struct {
@@ -54,6 +55,12 @@ type TelegramPlatformConfig struct {
 	ApiID             int    `json:"api_id"`
 	ApiHash           string `json:"api_hash"`
 	BotToken          string `json:"bot_token"`
+	PublicChannelName string `json:"public_channel_name"`
+}
+
+type LinePlatformConfig struct {
+	AccessToken       string `json:"access_token"`
+	AccessTokenSecret string `json:"access_token_secret"`
 	PublicChannelName string `json:"public_channel_name"`
 }
 
