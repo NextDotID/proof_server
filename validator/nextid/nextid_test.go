fee51c66cf48e6d69bee963d5503e1a73cd0926f
Nyk Ma <nykma@mask.io>
2023-09-26 16:33:29 +0800

    [+] validator/nextid: test

---
+++ validator/nextid/nextid_test.go
@@ -0,0 +1,50 @@
+package nextid
+
+import (
+	"crypto/ecdsa"
+	"testing"
+	"time"
+
+	"github.com/ethereum/go-ethereum/common"
+	"github.com/google/uuid"
+	"github.com/nextdotid/proof_server/types"
+	mycrypto "github.com/nextdotid/proof_server/util/crypto"
+	"github.com/nextdotid/proof_server/validator"
+	"github.com/stretchr/testify/require"
+)
+
+func GenerateNextIDTestData() (nextid *NextID, avatarSK, targetAvatarSK *ecdsa.PrivateKey) {
+	avatar, avatarSK := mycrypto.GenerateKeypair()
+	targetAvatar, targetAvatarSK := mycrypto.GenerateKeypair()
+	nextid = &NextID{
+		&validator.Base{
+			Platform:         types.Platforms.NextID,
+			Previous:         "",
+			Action:           types.Actions.Create,
+			Pubkey:           avatar,
+			Identity:         mycrypto.CompressedPubkeyHex(targetAvatar),
+			AltID:            "",
+			ProofLocation:    "",
+			Signature:        []byte{},
+			SignaturePayload: "",
+			Text:             "",
+			Extra:            map[string]string{"target_signature": ""},
+			CreatedAt:        time.Now(),
+			Uuid:             uuid.New(),
+		},
+	}
+
+	nextid.SignaturePayload = nextid.GenerateSignPayload()
+	nextid.Signature, _ = mycrypto.SignPersonal([]byte(nextid.SignaturePayload), avatarSK)
+	targetSig, _ := mycrypto.SignPersonal([]byte(nextid.SignaturePayload), targetAvatarSK)
+	nextid.Extra["target_signature"] = common.Bytes2Hex(targetSig)
+
+	return nextid, avatarSK, targetAvatarSK
+}
+
+func Test_Validate(t *testing.T) {
+	t.Run("success", func(t *testing.T) {
+		nextid, _, _ := GenerateNextIDTestData()
+		require.NoError(t, nextid.Validate())
+	})
+}
