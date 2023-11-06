60c825a26072e3f715c050a1acaba7c5e537c7ea
Nyk Ma <nykma@mask.io>
2023-11-06 17:53:26 +0800

    [#] model/subkey

--- model/subkey.go
+++ model/subkey.go
@@ -1,12 +1,21 @@
 package model
 
-import "time"
+import (
+	"time"
+
+	"github.com/nextdotid/proof_server/types"
+)
 
 type Subkey struct {
 	ID        int64     `gorm:"primarykey"`
 	CreatedAt time.Time `gorm:"not null"`
 	Name      string    `gorm:"not null"`
+	// Relying Party Identifier.
+	RP_ID string `gorm:"rp_id"`
 
-	Avatar string `gorm:"not null"`
-	Algorithm string `gorm:"algo"`
+	Avatar    string                `gorm:"not null"`
+	// Algorithm of this subkey
+	Algorithm types.SubkeyAlgorithm `gorm:"algorithm"`
+	// Public key of this subkey.
+	PublicKey string                `gorm:"public_key"`
 }
